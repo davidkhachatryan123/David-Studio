6fe8f7b89a2b788e2b4cb46bc3347eecfde4116f
davidkhachatryan123 <xdavit777@icloud.com>
2023-05-22 21:32:20 +0400

    Setup Admin Panel complete

--- src/Admin-Panel/src/app/app.module.ts
+++ src/Admin-Panel/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
