a9be44bf3298a83787d03b4499ba2573cbde3523
davidkhachatryan123 <xdavit777@icloud.com>
2023-05-22 21:38:40 +0400

    Update environment values

--- src/Admin-Panel/src/environments/environment.prod.ts
+++ src/Admin-Panel/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    api: 'http://${hostname}/api',
+  },
+  sessionStorageConfig: {
+    USER_KEY: 'auth-user',
+    TOKEN_KEY: 'token'
+  }
 };
--- src/Admin-Panel/src/environments/environment.ts
+++ src/Admin-Panel/src/environments/environment.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: false
+  production: false,
+  config: {
+    api: 'http://localhost:5000/api',
+  },
+  sessionStorageConfig: {
+    USER_KEY: 'auth-user',
+    TOKEN_KEY: 'token'
+  }
 };
