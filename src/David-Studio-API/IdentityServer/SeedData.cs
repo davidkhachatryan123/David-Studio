58d526b4554b5747ac66399f97b8445f04f408b3
davidkhachatryan123 <davidkhachatryan359@outlook.com>
2023-06-24 14:09:03 +0400

    Change seed data

--- src/David-Studio-API/IdentityServer/SeedData.cs
+++ src/David-Studio-API/IdentityServer/SeedData.cs
@@ -5,8 +5,11 @@ using IdentityServer.Database;
 using IdentityServer.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Polly;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace IdentityServer
 {
@@ -27,57 +30,69 @@ namespace IdentityServer
                 await configContext.Database.MigrateAsync();
 
                 var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-                var defaultUser = await userMgr.FindByNameAsync(configuration["DefaultUser:UserName"]!);
 
-                if (defaultUser == null)
-                {
-                    defaultUser = new ApplicationUser
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        UserName = configuration["DefaultUser:UserName"],
-                        Email = configuration["DefaultUser:Email"],
-                        EmailConfirmed = true,
-
-                    };
-
-                    var result = userMgr.CreateAsync(defaultUser, configuration["DefaultUser:Password"]!).Result;
-
-                    if (!result.Succeeded)
-                        throw new Exception(result.Errors.First().Description);
-
-                    logger.LogDebug("Default user created");
-                }
-                else
-                    logger.LogDebug("Default user already exists");
-
-                if (!configContext.Clients.Any())
-                {
-                    foreach (var client in Config.Clients(configuration))
-                        configContext.Clients.Add(client.ToEntity());
-                    configContext.SaveChanges();
-                }
-
-                if (!configContext.IdentityResources.Any())
-                {
-                    foreach (var resource in Config.IdentityResources)
-                        configContext.IdentityResources.Add(resource.ToEntity());
-                    configContext.SaveChanges();
-                }
-
-                if (!configContext.ApiResources.Any())
-                {
-                    foreach (var apiResource in Config.ApiResource)
-                        configContext.ApiResources.Add(apiResource.ToEntity());
-                    configContext.SaveChanges();
-                }
-
-                if (!configContext.ApiScopes.Any())
-                {
-                    foreach (var apiScope in Config.ApiScopes)
-                        configContext.ApiScopes.Add(apiScope.ToEntity());
-                    configContext.SaveChanges();
-                }
+                await EnsureSeedUsers(context, configuration, logger, userMgr);
+                await EnsureSeedData(configContext, configuration);
             });
+        }
+
+        private static async Task EnsureSeedUsers(
+            ApplicationDbContext context, IConfiguration configuration,
+            ILogger logger, UserManager<ApplicationUser> userMgr)
+        {
+            var defaultUser = await userMgr.FindByNameAsync(configuration["DefaultUser:UserName"]!);
+
+            if (defaultUser == null)
+            {
+                defaultUser = new ApplicationUser
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    UserName = configuration["DefaultUser:UserName"],
+                    Email = configuration["DefaultUser:Email"],
+                    EmailConfirmed = true,
+
+                };
+
+                var result = userMgr.CreateAsync(defaultUser, configuration["DefaultUser:Password"]!).Result;
+
+                if (!result.Succeeded)
+                    throw new Exception(result.Errors.First().Description);
+
+                logger.LogDebug("Default user created");
+            }
+            else
+                logger.LogDebug("Default user already exists");
+        }
+
+        private static async Task EnsureSeedData(ConfigurationDbContext context, IConfiguration configuration)
+        {
+            if (!context.Clients.Any())
+            {
+                foreach (var client in Config.Clients(configuration))
+                    await context.Clients.AddAsync(client.ToEntity());
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                foreach (var resource in Config.IdentityResources)
+                    await context.IdentityResources.AddAsync(resource.ToEntity());
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.ApiResources.Any())
+            {
+                foreach (var apiResource in Config.ApiResource)
+                    await context.ApiResources.AddAsync(apiResource.ToEntity());
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.ApiScopes.Any())
+            {
+                foreach (var apiScope in Config.ApiScopes)
+                    await context.ApiScopes.AddAsync(apiScope.ToEntity());
+                await context.SaveChangesAsync();
+            }
         }
 
         private static AsyncPolicy CreateRetryPolicy(IConfiguration configuration, Microsoft.Extensions.Logging.ILogger logger)
