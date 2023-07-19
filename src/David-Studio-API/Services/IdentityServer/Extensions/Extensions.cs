d6ec50ef1f6810f90b773b42ac6554d3611217fa
davidkhachatryan123 <davidkhachatryan359@outlook.com>
2023-07-19 18:45:25 +0400

    refactor(Identity Server): Add extensions

---
+++ src/David-Studio-API/Services/IdentityServer/Extensions/Extensions.cs
@@ -0,0 +1,84 @@
+﻿using Duende.IdentityServer.Configuration;
+using IdentityServer.Database;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IdentityServer.Extensions
+{
+    public static class Extensions
+    {
+        public static void AddDefaultIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequiredUniqueChars = 1;
+
+                // SignIn settings.
+                options.SignIn.RequireConfirmedEmail = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789;@$!%*?&=#";
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+        }
+
+        public static void AddDefaultIdentityServer(this IServiceCollection services, string connectionString, string? migrationsAssembly)
+        {
+            services.AddIdentityServer(options =>
+            {
+                options.IssuerUri = "null";
+
+                options.Events.RaiseSuccessEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+
+                // new key every 30 days
+                options.KeyManagement.RotationInterval = TimeSpan.FromDays(30);
+                // announce new key 2 days in advance in discovery
+                options.KeyManagement.PropagationTime = TimeSpan.FromDays(2);
+                // keep old key for 7 days in discovery for validation of tokens
+                options.KeyManagement.RetentionDuration = TimeSpan.FromDays(7);
+                // don't delete keys after their retention period is over
+                options.KeyManagement.DeleteRetiredKeys = false;
+                options.KeyManagement.SigningAlgorithms = new[]
+                {
+                    // RS256 for older clients (with additional X.509 wrapping)
+                    new SigningAlgorithmOptions(SecurityAlgorithms.RsaSha256) { UseX509Certificate = true },
+                    // PS256
+                    new SigningAlgorithmOptions(SecurityAlgorithms.RsaSsaPssSha256),
+                    // ES256
+                    new SigningAlgorithmOptions(SecurityAlgorithms.EcdsaSha256)
+                };
+            })
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                    builder.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                    builder.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                // this enables automatic token cleanup. this is optional.
+                options.EnableTokenCleanup = true;
+                // interval in seconds (default is 3600)
+                options.TokenCleanupInterval = 3600;
+            })
+            .AddAspNetIdentity<ApplicationUser>();
+        }
+    }
+}
--- src/David-Studio-API/Services/IdentityServer/Program.cs
+++ src/David-Studio-API/Services/IdentityServer/Program.cs
@@ -1,12 +1,7 @@
-﻿using Duende.IdentityServer.Configuration;
-using IdentityServer;
+﻿using IdentityServer;
 using IdentityServer.Database;
-using IdentityServer.Models;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
+using IdentityServer.Extensions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations.Internal;
-using Microsoft.IdentityModel.Tokens;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,71 +12,9 @@ var migrationsAssembly = typeof(Config).Assembly.GetName().Name;
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly)));
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-{
-    // Password settings.
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequiredLength = 8;
-    options.Password.RequiredUniqueChars = 1;
+builder.Services.AddDefaultIdentity();
 
-    // SignIn settings.
-    options.SignIn.RequireConfirmedEmail = true;
-
-    // User settings.
-    options.User.AllowedUserNameCharacters =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789;@$!%*?&=#";
-    options.User.RequireUniqueEmail = true;
-})
-.AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
-
-builder.Services.AddIdentityServer(options =>
-{
-    options.IssuerUri = "null";
-
-    options.Events.RaiseSuccessEvents = true;
-    options.Events.RaiseFailureEvents = true;
-    options.Events.RaiseErrorEvents = true;
-    options.Events.RaiseInformationEvents = true;
-
-    // new key every 30 days
-    options.KeyManagement.RotationInterval = TimeSpan.FromDays(30);
-    // announce new key 2 days in advance in discovery
-    options.KeyManagement.PropagationTime = TimeSpan.FromDays(2);
-    // keep old key for 7 days in discovery for validation of tokens
-    options.KeyManagement.RetentionDuration = TimeSpan.FromDays(7);
-    // don't delete keys after their retention period is over
-    options.KeyManagement.DeleteRetiredKeys = false;
-    options.KeyManagement.SigningAlgorithms = new[]
-    {
-        // RS256 for older clients (with additional X.509 wrapping)
-        new SigningAlgorithmOptions(SecurityAlgorithms.RsaSha256) { UseX509Certificate = true },
-        // PS256
-        new SigningAlgorithmOptions(SecurityAlgorithms.RsaSsaPssSha256),
-        // ES256
-        new SigningAlgorithmOptions(SecurityAlgorithms.EcdsaSha256)
-    };
-})
-.AddConfigurationStore(options =>
-{
-    options.ConfigureDbContext = builder =>
-        builder.UseSqlServer(connectionString,
-            sql => sql.MigrationsAssembly(migrationsAssembly));
-})
-.AddOperationalStore(options =>
-{
-    options.ConfigureDbContext = builder =>
-        builder.UseSqlServer(connectionString,
-            sql => sql.MigrationsAssembly(migrationsAssembly));
-
-    // this enables automatic token cleanup. this is optional.
-    options.EnableTokenCleanup = true;
-    // interval in seconds (default is 3600)
-    options.TokenCleanupInterval = 3600;
-})
-.AddAspNetIdentity<ApplicationUser>();
+builder.Services.AddDefaultIdentityServer(connectionString, migrationsAssembly);
 
 builder.Services.AddAuthorization();
 
