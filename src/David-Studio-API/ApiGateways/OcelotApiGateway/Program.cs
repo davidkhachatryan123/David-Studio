261dd796e8861b55ba556660e750cd26ca6c2f4d
davidkhachatryan123 <davidkhachatryan359@outlook.com>
2023-08-30 10:05:19 +0400

    feat(API GW): Create API Gateway for Web app

---
+++ src/David-Studio-API/ApiGateways/OcelotApiGateway/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var identityUrl = builder.Configuration.GetValue<string>("IdentityUrl");
+var authenticationProviderKey = "IdentityApiKey";
+
+builder.Services.AddAuthentication()
+    .AddJwtBearer(authenticationProviderKey, x =>
+    {
+        x.Authority = identityUrl;
+        x.RequireHttpsMetadata = false;
+        x.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidAudiences = new[] { "users", "portfolio", "pricing", "storage", "messenger" }
+        };
+    });
+
+IConfiguration configuration = builder.Configuration
+    .AddJsonFile(Path.Combine("configuration", "configuration.json"), false, true)
+    .Build();
+
+builder.Services.AddOcelot(configuration);
+
+var app = builder.Build();
+
+app.UseOcelot().Wait();
+
+app.Run();
