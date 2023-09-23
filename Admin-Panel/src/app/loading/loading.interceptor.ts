f2a7ae27081b56511a7dda4571d0381444fbced2
davidkhachatryan123 <davidkhachatryan359@gmail.com>
2023-09-23 17:32:17 +0400

    feat(Admin Panel): Add exclude list of urls for http interceptor

--- Admin-Panel/src/app/loading/loading.interceptor.ts
+++ Admin-Panel/src/app/loading/loading.interceptor.ts
@@ -3,26 +3,45 @@ import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/c
 import { Observable, finalize } from 'rxjs';
 
 import { LoadingService } from './loading.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
   private totalRequests = 0;
+  private excludeUrls: Array<string>;
 
-  constructor(private loadingService: LoadingService) { }
+  constructor(private loadingService: LoadingService) {
+    this.excludeUrls = [
+      `${environment.identity.authority}/.+`
+    ];
+   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.totalRequests++;
+    if (this.isValidRequestForInterceptor(req.url)) {
+      this.totalRequests++;
+      this.loadingService.setLoading(true);
 
-    this.loadingService.setLoading(true);
+      return next.handle(req).pipe(
+        finalize(() => {
+          this.totalRequests--;
 
-    return next.handle(req).pipe(
-      finalize(() => {
-        this.totalRequests--;
+          if (this.totalRequests == 0) {
+            this.loadingService.setLoading(false);
+          }
+        })
+      );
+    }
 
-        if (this.totalRequests == 0) {
-          this.loadingService.setLoading(false);
-        }
-      })
-    );
+    return next.handle(req);
+  }
+
+  private isValidRequestForInterceptor(requestuUrl: string): boolean {
+    for (let address of this.excludeUrls) {
+      if (new RegExp(address).test(requestuUrl)) {
+        return false;
+      }
+    }
+    
+    return true;
   }
 }
